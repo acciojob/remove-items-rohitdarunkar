cf1ce599d1ebf89af33295a8218af52f5dcbd36e
Rohit Darunkar <139346362+rohitdarunkar@users.noreply.github.com>
2026-01-17 17:58:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const btn = document.querySelector('input[type="button"]');
+
+btn.addEventListener("click", function () {
+  const select = document.getElementById("colorSelect");
+
+  // Remove selected option
+  if (select.selectedIndex !== -1) {
+    select.remove(select.selectedIndex);
+  }
+});
